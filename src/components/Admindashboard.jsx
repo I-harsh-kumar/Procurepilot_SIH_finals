275317e5f842661de70157c5f1d6bd4a398f223e
SURAJ <90304648+SURAJ-SHARMA27@users.noreply.github.com>
2023-12-19 01:37:30 +0530

    drawer fixed-1

--- src/components/Admindashboard.jsx
+++ src/components/Admindashboard.jsx
@@ -621,6 +621,7 @@ useEffect(() => {
 
                 <div style={{ fontSize: '14px', width: '30%' }} className="table-cell">
                   <div>Updated By</div>
+              
                   <div>{item.updatedBy}</div>
                   <div className="">Updated Start</div>
                   <div>{item.updatedStart}</div>
@@ -835,7 +836,8 @@ useEffect(() => {
         <Modal.Body>
           <Table striped bordered hover>
             <thead>
-              <tr>
+              <tr>  
+                
                 <th>Approval Stage</th>
                 <th>Name of Officer</th>
                 <th>Action</th>
